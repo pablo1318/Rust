4af8d0140b79983ea033e0a1125d35b12916dd93
pablo1318 <lajotacambra2@gmail.com>
2022-07-15 22:10:44 +0200

    first commit calculator works

---
+++ rust-calculator/target/debug/deps/rust_calculator-501e9957da63c6a8.d
@@ -0,0 +1,5 @@
+/home/pablocambra/Rust/rust-calculator/target/debug/deps/rust_calculator-501e9957da63c6a8: src/main.rs
+
+/home/pablocambra/Rust/rust-calculator/target/debug/deps/rust_calculator-501e9957da63c6a8.d: src/main.rs
+
+src/main.rs:
---
+++ rust-calculator/target/debug/rust-calculator.d
@@ -0,0 +1,1 @@
+/home/pablocambra/Rust/rust-calculator/target/debug/rust-calculator: /home/pablocambra/Rust/rust-calculator/src/main.rs
