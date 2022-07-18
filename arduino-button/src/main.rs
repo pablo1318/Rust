4178be6fd47385fbffce9aa2e99bee86c07ed117
pablo1318 <lajotacambra2@gmail.com>
2022-07-18 19:31:26 +0200

    cambiado booleano pulsado porque se bugeaba

--- arduino-button/src/main.rs
+++ arduino-button/src/main.rs
@@ -1,17 +1,12 @@
 #![no_std]
 #![no_main]
 
-
 use panic_halt as _;
 use ufmt::{self, uwrite};
 
-
-const BAUD_RATE:u32 = 57600; //BAUD RATE FOR SERIAL PRINTING
+const BAUD_RATE: u32 = 57600; //BAUD RATE FOR SERIAL PRINTING
 #[arduino_hal::entry]
 fn main() -> ! {
-    
-    
-    
     //SETUP
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
@@ -19,20 +14,17 @@ fn main() -> ! {
     let but = pins.d12.into_pull_up_input();
     let mut serial = arduino_hal::default_serial!(dp, pins, BAUD_RATE);
 
-
-
     //BEGIN
-    let mut pulsado  = false;
+    let mut pulsado = true;
     led.set_low();
-    
-    
+
     loop {
-        if but.is_high() && !pulsado{
+        if but.is_high() && !pulsado {
             led.set_low();
             pulsado = true;
             _ = ufmt::uwriteln!(&mut serial, "APAGAMOS EL LED");
         }
-        if but.is_low() && pulsado{
+        if but.is_low() && pulsado {
             led.set_high();
             pulsado = false;
             _ = ufmt::uwriteln!(&mut serial, "ENCENDEMOS EL LED");
